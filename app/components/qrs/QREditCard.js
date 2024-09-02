2c11329e8bb30b132facad9526279cfa178ec79d
Ethan Storm <github.balsamic226@passinbox.com>
2024-09-01 18:25:45 -0700

    begin edit page

---
+++ app/components/qrs/QREditCard.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+import Image from "next/image";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/20/solid";
+
+export default function QRCard({ qr, index }) {
+  return (
+    <li
+      className="flex flex-wrap items-center justify-center w-full"
+      key={`${qr.url}/${qr.code}/${index}`}
+    >
+      <div className="flex-shrink-0 m-6 relative overflow-hidden bg-purple-500 rounded-lg max-w-60 shadow-lg">
+        <button>Edit</button>
+        <svg
+          className="absolute bottom-0 left-0 mb-8"
+          viewBox="0 0 375 283"
+          fill="none"
+          style={{ transform: "scale(1.5)", opacity: 0.1 }}
+        >
+          <rect
+            x="159.52"
+            y="175"
+            width="152"
+            height="152"
+            rx="8"
+            transform="rotate(-45 159.52 175)"
+            fill="white"
+          />
+          <rect
+            y="107.48"
+            width="152"
+            height="152"
+            rx="8"
+            transform="rotate(-45 0 107.48)"
+            fill="white"
+          />
+        </svg>
+        <div className="pt-10 px-10 flex items-center justify-center">
+          <Image
+            className=" w-40"
+            src={qr.qr}
+            alt=""
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className="text-white p-4 w-full">
+          <span className="opacity-75 break-words line-clamp-3 max-w-fit overflow-ellipsis">
+            {qr.url}
+          </span>
+          <div className="flex justify-between">
+            <span className="block font-semibold text-xl">
+              {qr.protected ? (
+                <EyeSlashIcon
+                  aria-hidden="true"
+                  className="mr-1.5 h-5 w-5 flex-shrink-0 text-black-400"
+                />
+              ) : (
+                <EyeIcon
+                  aria-hidden="true"
+                  className="mr-1.5 h-5 w-5 flex-shrink-0 text-black-400"
+                />
+              )}
+            </span>
+            <span>{qr.count}</span>
+            <span className="block bg-white rounded-full text-purple-500 text-xs font-bold px-3 py-2 leading-none flex items-center">
+              {qr.code}
+            </span>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+}
---
+++ app/edit/page.js
@@ -0,0 +1,105 @@
+"use client";
+
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+
+import QREditCard from "../components/qrs/QREditCard";
+
+export default function AllQRS({ params }) {
+  const { register, handleSubmit } = useForm();
+  const [error, setError] = useState("");
+  const allQRS = [
+    {
+      url: "https://youtube.com",
+      code: "Cool!",
+      protected: false,
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Pencil_drawing_of_a_girl_in_ecstasy.jpg/220px-Pencil_drawing_of_a_girl_in_ecstasy.jpg",
+    },
+  ];
+
+  const postSearch = async (formData) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/qr/code`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(formData),
+    });
+
+    const body = await response.json();
+
+    setError(body.error);
+
+    if (body.codeQRs) {
+      replaceQRData(body);
+    }
+  };
+
+  return (
+    <div className="my-10 w-full">
+      <div className="py-6 flex flex-col justify-center w-full sm:py-12">
+        <div className="relative py-3 sm:max-w-2xl sm:mx-auto w-full">
+          <div className="absolute inset-0 w-full bg-gradient-to-r from-indigo-900 to-purple-500 shadow-lg transform -skew-y-6 sm:skew-y-0 -rotate-1 sm:-rotate-6 rounded-3xl"></div>
+          <div className="text-white relative px-2 py-10 bg-indigo-500 shadow-lg rounded-3xl sm:p-10 max-w-3xl">
+            <form
+              className="w-full mb-5"
+              onSubmit={handleSubmit((data) => {
+                postSearch(data);
+              })}
+            >
+              <label
+                htmlFor="code"
+                className="block text-lg font-medium leading-6 text-white"
+              >
+                Search by Group Code:
+              </label>
+              <div className="relative mt-2 rounded-md shadow-sm">
+                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                  <span className="text-gray-500 sm:text-sm">#</span>
+                </div>
+                <input
+                  {...register("code")}
+                  type="text"
+                  name="code"
+                  id="code"
+                  className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="something fancy!"
+                />
+
+                <div className="absolute inset-y-0 right-0 flex items-center p4-3 text-gray-600">
+                  <button type="submit">
+                    <MagnifyingGlassIcon
+                      aria-hidden="true"
+                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                    />
+                  </button>
+                </div>
+              </div>
+            </form>
+
+            <ul
+              role="list"
+              className="divide-y divide-gray-100 scrollbar w-full overflow-x-hidden"
+              style={{ maxHeight: "63em", overflowY: "scroll" }}
+            >
+              {allQRS?.length > 0 ? (
+                allQRS.map((qr, index) => (
+                  <QREditCard
+                    key={`${qr.code}-${index}`}
+                    qr={qr}
+                    index={index}
+                  />
+                ))
+              ) : (
+                <div className="my-10">
+                  <p className="">There are no QRs here</p>
+                </div>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
