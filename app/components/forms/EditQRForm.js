2821416d9232cfc30da506f128c7d5c8784d672a
Ethan Storm <github.balsamic226@passinbox.com>
2024-09-05 23:27:00 -0700

    componetize new edit and delete btns, and create bttn

--- app/components/forms/EditQRForm.js
+++ app/components/forms/EditQRForm.js
@@ -3,6 +3,8 @@ import { useForm } from "react-hook-form";
 import { PopoverButton } from "@headlessui/react";
 import createRandomString from "../lib";
 import CloseButton from "../menus/CloseButton";
+import EditButton from "../menus/CreateButton";
+import DeleteButton from "../menus/DeleteButton";
 import LoadingSpinner from "../misc/LoadingSpinner";
 
 export default function EditQRForm({ selectedQRCode }) {
@@ -215,25 +217,13 @@ export default function EditQRForm({ selectedQRCode }) {
       </div>
 
       <div className="mt-6 flex gap-5 items-center justify-startgap-x-6">
-        <button
-          type="submit"
-          disabled={loading}
-          className="relative inline-block text-lg group"
-        >
-          <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-blue-900 rounded-lg group-hover:text-white">
-            <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-            <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-            <span className="relative">Create</span>
-          </span>
-          <span
-            className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-blue-700 rounded-lg group-hover:mb-0 group-hover:mr-0"
-            data-rounded="rounded-lg"
-          ></span>
-        </button>
+        <EditButton loading={loading} />
+        <DeleteButton loading={loading} />
         <LoadingSpinner loading={loading} />
         <PopoverButton className="my-5 inline-flex font-semibold leading-6 text-gray-900">
           <CloseButton />
         </PopoverButton>
+
         <label className="text-red-500 mx-3">{error}</label>
       </div>
     </form>
